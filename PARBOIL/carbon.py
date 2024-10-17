fca191c78881e873f0566c7d1a77dc3f8ef51f68
arthcs <arth.csil@gmail.com>
2024-10-17 15:55:27 -0300

    Parboil

---
+++ PARBOIL/carbon.py
@@ -0,0 +1,37 @@
+from carbontracker.tracker import CarbonTracker
+import os
+import sys
+
+max_epochs = 1
+num_application = 0
+threads_num = sys.argv[1] #parâmetro do bash
+benchmark = sys.argv[2]
+version = sys.argv[3]
+data_set = sys.argv[4]
+log_dir = './out/'+str(benchmark)+'/'
+
+tracker = CarbonTracker(
+        epochs=1,
+        decimal_precision=4,
+        verbose=1,
+        log_dir=str(log_dir),
+        log_file_prefix='parboil_'+str(benchmark)+'_'+str(threads_num)+'T',
+        ignore_errors=True
+)
+
+# Training loop.
+for epoch in range(max_epochs):
+    
+    print("\n Start Clean")
+    os.system('python2 parboil clean '+str(benchmark))
+    print("\n Start Compile")
+    os.system('python2 parboil compile '+str(benchmark)+' '+str(version))
+
+    tracker.epoch_start()
+    os.system('OMP_NUM_THREADS='+str(threads_num)+' python2 parboil run '+str(benchmark)+' '+str(version)+' '+str(data_set))
+    tracker.epoch_end()
+
+# Optional: Add a stop in case of early termination before all monitor_epochs has
+# been monitored to ensure that actual consumption is reported.
+
+tracker.stop()
--- hpc-benchmarks-main/parboil/execute.sh
+++ hpc-benchmarks-main/parboil/execute.sh
@@ -3,20 +3,36 @@
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores
 
-./parboil clean bfs
-./parboil compile bfs omp_base
-./parboil clean lbm
-./parboil compile lbm omp_cpu
+python2 parboil clean bfs
+python2 parboil compile bfs omp_base
+python2 parboil clean lbm
+python2 parboil compile lbm omp_cpu
 
-for nt in 1 2 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120 124 128
+for nt in 80 84 88
 do
 	echo "Running with $nt threads"
 	export OMP_NUM_THREADS=$nt
 	for iter in 1 2 3
 	do
 		echo "Running bfs with $nt threads"
-		./parboil run bfs omp_base SF >> out/bfs.$nt.txt
+		python2 parboil run bfs omp_base SF >> out/bfs.$nt.txt
+
+		echo "Running histo with $nt threads"
+		python2 parboil run histo omp_cpu long >> out/histo.$nt.txt
+
 		echo "Running lbm with $nt threads"
-		./parboil run lbm omp_cpu long >> out/lbm.$nt.txt
+		python2 parboil run lbm omp_cpu long >> out/lbm.$nt.txt
+
+		echo "Running mrio with $nt threads"
+		python2 parboil run mrio omp_cpu long >> out/mrio.$nt.txt
+
+		echo "Running cutcp with $nt threads"
+		python2 parboil run cutcp omp_cpu long >> out/cutcp.$nt.txt
 	done
 done
+
+# BFS
+# HISTO
+# LBM
+# MRIQ
+# CUTCP
