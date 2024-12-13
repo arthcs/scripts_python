5d99e9fb692ae6a746d45e042b0b056096322d1d
arthcs <arth.csil@gmail.com>
2024-12-13 16:30:06 -0300

    Proque vidro tem gosto e sangue?

--- RODINIA/carbon.py
+++ RODINIA/carbon.py
@@ -5,41 +5,27 @@ import sys
 max_epochs = 1
 num_application = 0
 threads_num = sys.argv[1] #parâmetro do bash
-benchmark = sys.argv[2]
-version = sys.argv[3]
-data_set = sys.argv[4]
-log_dir = './out/'+str(benchmark)+'/'
+benchmark_argv = sys.argv[2]
+
+log_dir = './out/'
 
 tracker = CarbonTracker(
         epochs=1,
         decimal_precision=4,
-        verbose=1,
+        verbose=0,
         log_dir=str(log_dir),
-        log_file_prefix='rodinia_'+str(benchmark)+'_'+str(threads_num)+'T',
+        log_file_prefix='rodinia_'+str(threads_num)+'T',
         ignore_errors=True
 )
 
 # Training loop.
 for epoch in range(max_epochs):
-    
-    print("\n Start Clean")
-    os.system('python2 parboil clean '+str(benchmark))
-    print("\n Start Compile")
-    os.system('python2 parboil compile '+str(benchmark)+' '+str(version))
-
+    print('\n OMP_NUM_THREADS='+str(threads_num)+' '+str(benchmark_argv))
     tracker.epoch_start()
-    os.system('OMP_NUM_THREADS='+str(threads_num)+' python2 parboil run '+str(benchmark)+' '+str(version)+' '+str(data_set))
+    os.system('OMP_NUM_THREADS='+str(threads_num)+' '+str(benchmark_argv))
     tracker.epoch_end()
 
 # Optional: Add a stop in case of early termination before all monitor_epochs has
 # been monitored to ensure that actual consumption is reported.
 
 tracker.stop()
-
-# Usage: ./hotspot <grid_rows> <grid_cols> <sim_time> <no. of threads><temp_file> <power_file>
-#         <grid_rows>             - number of rows in the grid (positive integer)
-#         <grid_cols>             - number of columns in the grid (positive integer)
-#         <sim_time>              - number of iterations
-#         <no. of threads>    - number of threads
-#         <temp_file>             - name of the file containing the initial temperature values of each cell
-#         <power_file>            - name of the file containing the dissipated power values of each cell
