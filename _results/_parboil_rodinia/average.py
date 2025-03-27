849f992106cad3abc64836a39e76491d85ec5546
arthcs <arth.csil@gmail.com>
2025-03-27 11:43:43 -0300

    media em python

---
+++ _results/_parboil_rodinia/average.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import glob
+
+def do_average(arc, app):
+    # Lista todos os arquivos CSV no diretório atual
+    csv_files = glob.glob("process_data/"+arc+"/"+app+"*.csv")[:10]  # Garante que apenas 10 arquivos sejam usados
+    #print(csv_files)
+
+    if csv_files:
+        dataframes = []
+
+        # Lê todos os arquivos CSV e os armazena em uma lista
+        to_average_columns = ["Time", "Energy", "CO2eq", "travelled"]
+
+        for file in csv_files:
+            df = pd.read_csv(file)
+            dataframes.append(df)
+
+        # Concatena todos os DataFrames
+        combined_df = pd.concat(dataframes)
+
+        # Divide a coluna Time por 1000 para converter milissegundos em segundos
+        #combined_df["Time"] /= 1000
+
+        # Calcula a média dos valores numéricos
+        mean_df = combined_df.groupby(["Treads", "App"])[to_average_columns].mean().round(4).reset_index()
+
+        # Salva o resultado em um novo arquivo CSV
+        mean_df.to_csv("process_data/"+arc+"/average_"+app+".csv", index=False)
+
+        print("Arquivo 'average_"+app+".csv' gerado com sucesso! Salvo em "+arc)
+    else:
+        print("Nenhum arquivo encontrado para "+arc+" e "+app)
+
+arc = ["blaise", "hype", "phoenix", "tupi"]
+app = ["PO", "ST", "bfs", "cutcp", "histo", "lbm", "mri"]
+
+for a in arc:
+    for ap in app:
+        do_average(a, ap)
---
+++ _results/balise_PO/log_script_v4.py
@@ -0,0 +1,148 @@
+import pandas as pd
+import re
+import os
+
+def extract_number(line):
+    match = re.search(r'com (\d+) Treads', line)
+    if match:
+        return int(match.group(1))
+    return None
+
+# Função para substituir múltiplos espaços por '|'
+def substituir_espacos(texto):
+    linhas = texto.splitlines()  # Separar o texto por linhas
+    linhas_modificadas = [','.join(linha.split()) for linha in linhas]  # Substituir espaços por '|' em cada linha
+    return '\n'.join(linhas_modificadas)  # Reunir as linhas com quebra de linha
+
+def process_data(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    data_per_app = {}  # Dicionário para armazenar os dataframes separados por App
+    entry = {}
+
+    for line in lines:
+        line = line.strip()
+        if line.startswith("___") and "Treads___" in line:
+            if entry:  # Salva a entrada anterior no dicionário
+                app_name = entry.get('App', 'Unknown')
+                if app_name not in data_per_app:
+                    data_per_app[app_name] = []
+                data_per_app[app_name].append(entry)
+
+            entry = {}  # Inicia uma nova entrada
+            entry['Treads'] = extract_number(line)
+
+            if line.startswith("___Executando "):
+                match = re.search(r"___Executando ([a-zA-Z]+)", line)
+            else:
+                match = re.search(r"___([a-zA-Z]+)", line)
+
+            if match:
+                entry['App'] = match.group(1)
+        # Tempo adicionado pelo python
+        elif line.startswith("Tempo total:"):  # Parboil milisegundos
+            #print(line)
+            match = re.search(r'(\d+.\d+) ms', line)
+            #print(match.group(1))
+            if match:
+                entry['Time'] = float(match.group(1))
+        #EDITAR: encorar o tempo retornado pelo app
+        elif line.startswith("Timer Wall Time"):  # Parboil milisegundos
+            entry['Time'] = float(line.split(':')[1].strip())
+        elif line.startswith("Total time:"):  # Rodinia segundos
+            entry['Time'] = float(line.split(':')[1].replace('seconds', '').strip())
+        elif line.startswith("Time consumed(ms):"):  # Rodinia milisegundos
+            entry['Time'] = float(line.split(':')[1].strip())
+        elif line.startswith("Total application run time: "):  # leukocyte
+             seg = float(line.split(':')[1].replace('seconds', '').strip())
+             entry['Time'] = seg #* 1000 # multiplicar por 1000 para converter para milissegundos
+        elif line.startswith("Time for process: "): # kmeans
+            entry['Time'] = float(line.split(':')[1].strip())
+        elif line.startswith("Compute time: "): # cfd
+            entry['Time'] = float(line.split(':')[1].strip())
+            # backprop sem saida
+            # rad Sem saida
+
+        #Carbon Tracker
+        elif line.startswith("Time:"):  # Time CT
+            entry['TimeCT'] = line.split('\t')[1].strip()
+        elif line.startswith("Energy:"):
+            match = re.search(r"([0-9.]+) kWh", line)
+            if match:
+                entry['Energy'] = float(match.group(1))
+        elif line.startswith("CO2eq:"):
+            match = re.search(r"([0-9.]+) g", line)
+            if match:
+                entry['CO2eq'] = float(match.group(1))
+        elif line.endswith("km travelled by car"):
+            match = re.search(r"([0-9.]+) km", line)
+            if match:
+                entry['km travelled by car'] = float(match.group(1))
+
+    # Adiciona a última entrada ao dicionário
+    if entry:
+        app_name = entry.get('App', 'Unknown')
+        if app_name not in data_per_app:
+            data_per_app[app_name] = []
+        data_per_app[app_name].append(entry)
+
+    # Criar um dicionário com DataFrames para cada app
+    df_per_app = {app: pd.DataFrame(entries) for app, entries in data_per_app.items()}
+
+    return df_per_app
+
+folder = "data"
+
+if not os.path.exists(folder):
+    print(f"Erro: Pasta data não encontrada!")
+    exit()
+
+
+if not os.path.exists("process_data"):
+    os.makedirs("process_data")
+    print(f"Pasta process_data criada.")
+else:
+    print(f"Pasta process_data já existe.")
+
+# busca o nome dos arquivos que terminam com .out
+files_out = [arquivo for arquivo in os.listdir("./"+folder) if arquivo.endswith('.out')]
+print(files_out)
+#exit()
+
+for file in files_out:
+    #print("Processando arquivo: "+file)
+
+    #####Organiza os dados
+    # Caminho para o arquivo de dados
+    file_name = file
+    print(file)
+    #file_name = 'log_limpo_'+ str(num+1) + '.txt'
+
+    # Processar os dados e criar a tabela
+    #df = process_data('data/'+file_name)
+    df_per_app = process_data(folder+'/'+file_name)
+
+    for app, df in df_per_app.items():
+        print(f"DataFrame para {app}:")
+        #print(df)
+        df_str = df.to_string(index=False)
+        csv = substituir_espacos(df_str)
+        csv = csv.replace("km,travelled,by,car", "travelled")
+
+        # Verifica a pasta da arquitetura ou cria a pasta
+        pasta = re.match(r"^[a-zA-Z]+", file_name)
+        pasta = pasta.group()
+        if not os.path.exists("process_data/" + pasta):
+            os.makedirs("process_data/" + pasta)
+            print(f"Pasta '{pasta}' criada.")
+
+        num = re.search(r"\d+", file_name)
+        output_file = 'process_data/' + pasta + '/' + app + "_" + num.group()+".csv"
+
+        with open(output_file, 'w') as f:
+            f.write(csv)
+
+        print(f"Os resultados foram salvos em {output_file}")
+
+print("Finalizado process_data")
